f2fc70a869a1a07a7907bafd3d91204f38b5f972
Ian Sowinski <sowinj@gmail.com>
2017-03-21 22:13:23 +0100

    test for existence

--- spec/classes_spec.rb
+++ spec/classes_spec.rb
@@ -9,4 +9,18 @@ describe Country  do
                 "",
                 "")
   end
+  describe "instance variables" do
+    it "should be equal as in :country initialize" do
+      country.name.should == "Poland"
+      country.type.should == "land"
+      country.neighbours.should == ["ger", "rus", "cze", "slo", "lit", "bel", "ukr"]
+      country.army_type.should == ""
+      country.belongs_to.should == ""
+    end
+  end
+  describe "classes.id" do 
+    it "should be a symbol" do
+      country.id.class.should == Symbol
+    end
+  end
 end
