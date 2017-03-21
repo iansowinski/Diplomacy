18b213cbf98189127b2bd4551a4fca1104be5978
Ian Sowinski <sowinj@gmail.com>
2017-03-21 22:14:00 +0100

    initialize working properly

--- lib/classes.rb
+++ lib/classes.rb
@@ -1,5 +1,12 @@
 class Country
+  attr_reader :id, :name, :type, :neighbours, :army_type, :belongs_to
 
   def initialize (id, name, type, neighbours, army_type, belongs_to)
+    @id = id.to_sym
+    @name = name
+    @type = type
+    @neighbours = neighbours
+    @army_type = army_type
+    @belongs_to = belongs_to
   end
 end
