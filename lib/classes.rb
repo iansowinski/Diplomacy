7cbbd5f53299a2974b3f592fa07a10051076d56f
Ian Sowinski <sowinj@gmail.com>
2017-03-22 17:27:27 +0100

    Country.new is now passing tests

--- lib/classes.rb
+++ lib/classes.rb
@@ -1,12 +1,45 @@
 class Country
-  attr_reader :id, :name, :type, :neighbours, :army_type, :belongs_to
-
+  
+  attr_reader :id, :name, :type, :neighbours
+  attr_accessor :army_type, :belongs_to
+  
   def initialize (id, name, type, neighbours, army_type, belongs_to)
-    @id = id.to_sym
-    @name = name
-    @type = type
-    @neighbours = neighbours
-    @army_type = army_type
-    @belongs_to = belongs_to
+    if id.class  == Symbol and id.length == 3
+      @id = id.to_sym
+    else
+      raise(ArgumentError)
+    end
+    if name.class == String and name.length > 0
+      @name = name
+    else
+      raise(ArgumentError)
+    end
+    if type == :land or type == :water or type == :land_with_water
+      @type = type
+    else
+      raise(ArgumentError)
+    end
+    if neighbours.class == Array and neighbours.size > 0
+      neighbours.each do |neighbour|
+        if neighbour.class != Symbol or neighbour.size != 3
+          raise (ArgumentError) 
+        end
+      end
+      @neighbours = neighbours
+    else
+      raise(ArgumentError)
+    end
+    if army_type == :army or army_type == :fleet or army_type == nil
+      @army_type = army_type
+    else
+      raise(ArgumentError)
+    end
+    if [:germany, :russia, :austria, :turkey, :england, :france, :italy, nil].include?(belongs_to) == true
+      @belongs_to = belongs_to
+    else
+      raise(ArgumentError)
+    end
   end
+
 end
+
